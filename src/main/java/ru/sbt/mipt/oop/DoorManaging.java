5922e95fb174e99cd0c48bf7db7c4fd779613b4f
Borisova Olga <31622142+Bordoglor@users.noreply.github.com>
2020-11-16 18:35:28 +0300

    Update DoorManaging.java

--- src/main/java/ru/sbt/mipt/oop/DoorManaging.java
+++ src/main/java/ru/sbt/mipt/oop/DoorManaging.java
@@ -19,24 +19,10 @@ public class DoorManaging implements Controllable {
                         } else {
                             door.setOpen(false);
                             System.out.println("Door " + door.getId() + " was closed.");
-                        }
+ }
                     }
                 }
             });
         }
     }
-<<<<<<< Updated upstream
-
-    private void allLightOff(SmartHome smartHome) {
-        for (Room homeRoom : smartHome.getRooms()) {
-            for (Light light : homeRoom.getLights()) {
-                light.setOn(false);
-                SensorCommand command = new SensorCommand(CommandType.LIGHT_OFF, light.getId());
-                sendCommand(command);
-            }
-        }
-    }
 }
-=======
-}
->>>>>>> Stashed changes
