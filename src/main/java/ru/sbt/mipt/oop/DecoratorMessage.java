0731a7cfc8d907527938c4fe54be98718b86c587
Borisova Olga <31622142+Bordoglor@users.noreply.github.com>
2020-11-16 18:32:54 +0300

    Update DecoratorMessage.java

--- src/main/java/ru/sbt/mipt/oop/DecoratorMessage.java
+++ src/main/java/ru/sbt/mipt/oop/DecoratorMessage.java
@@ -2,36 +2,16 @@ package ru.sbt.mipt.oop;
 
 import static ru.sbt.mipt.oop.SensorEventType.*;
 
-<<<<<<< Updated upstream
-public class DecoratorMessage implements Manageable{
-
-    private Manageable manageable;
-
-    public DecoratorMessage(Manageable manageable) {
-        this.manageable = manageable;
-=======
 public class DecoratorMessage implements Controllable {
 
     private Controllable controllable;
 
     public DecoratorMessage(Controllable controllable) {
         this.controllable = controllable;
->>>>>>> Stashed changes
     }
 
     @Override
     public void manage(SmartHome smartHome, SensorEvent event) {
-<<<<<<< Updated upstream
-        if (! (event.getType() == DOOR_OPEN || event.getType() == DOOR_CLOSED ||
-                event.getType() == LIGHT_ON || event.getType() == LIGHT_OFF)){
-            return;
-        }
-        if (smartHome.getState() instanceof AlarmDeactive){
-            manageable.manage(smartHome, event);
-        } else if (smartHome.getState() instanceof AlarmAlert){
-            System.out.println("Sending sms");
-        }
-=======
         Alarm alarm = smartHome.alarm;
         if (!(event.getType() == ALARM_ACTIVATE || event.getType() == ALARM_DEACTIVATE)){
             System.out.println("Sending sms");
@@ -39,6 +19,5 @@ public class DecoratorMessage implements Controllable {
         if (smartHome.getState() instanceof AlarmDeactive) {
             controllable.manage(smartHome, event);
         }
->>>>>>> Stashed changes
     }
 }
