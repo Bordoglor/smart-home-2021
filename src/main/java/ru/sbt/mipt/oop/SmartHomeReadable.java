c06eb30552add21745a3e135adb1262a3681e92e
Borisova Olga <31622142+Bordoglor@users.noreply.github.com>
2020-11-16 18:44:37 +0300

    Update SmartHomeReadable.java

--- src/main/java/ru/sbt/mipt/oop/SmartHomeReadable.java
+++ src/main/java/ru/sbt/mipt/oop/SmartHomeReadable.java
@@ -3,9 +3,5 @@ package ru.sbt.mipt.oop;
 import java.io.IOException;
 
 public interface SmartHomeReadable {
-<<<<<<< Updated upstream
-    public SmartHome getSmartHome() throws IOException;
-=======
     public SmartHome getSmartHome();
->>>>>>> Stashed changes
 }
