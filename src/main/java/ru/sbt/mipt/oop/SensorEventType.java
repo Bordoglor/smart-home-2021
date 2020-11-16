cee6185c22478b98b1cfdabcc350d324c42f2f98
Borisova Olga <31622142+Bordoglor@users.noreply.github.com>
2020-11-16 18:42:04 +0300

    Update SensorEventType.java

--- src/main/java/ru/sbt/mipt/oop/SensorEventType.java
+++ src/main/java/ru/sbt/mipt/oop/SensorEventType.java
@@ -2,19 +2,4 @@ package ru.sbt.mipt.oop;
 
 public enum SensorEventType {
     LIGHT_ON, LIGHT_OFF, DOOR_OPEN, DOOR_CLOSED, ALARM_ACTIVATE, ALARM_DEACTIVATE;
-<<<<<<< Updated upstream
-
-    private String code;
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        if (this == ALARM_ACTIVATE || this == ALARM_DEACTIVATE){
-            this.code = code;
-        }
-    }
-=======
->>>>>>> Stashed changes
 }
