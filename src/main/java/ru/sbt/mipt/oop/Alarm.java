25edf8a81ec075decbbd04ea3c5bfc80e0e394e4
Borisova Olga <31622142+Bordoglor@users.noreply.github.com>
2020-11-16 18:58:30 +0300

    Update Alarm.java

--- src/main/java/ru/sbt/mipt/oop/Alarm.java
+++ src/main/java/ru/sbt/mipt/oop/Alarm.java
@@ -1,15 +1,5 @@
 package ru.sbt.mipt.oop;
 
-<<<<<<< Updated upstream
-public class Alarm implements State, Actionable{
-
-    private String code;
-    private State state;
-
-    public Alarm() {
-        this.code = "0";
-        this.state = new AlarmDeactive();
-=======
 public class Alarm implements AlarmState, Actionable{
 
     private String code;
@@ -18,7 +8,6 @@ public class Alarm implements AlarmState, Actionable{
     public Alarm() {
         this.code = "0";
         this.alarmState = new AlarmDeactive();
->>>>>>> Stashed changes
     }
 
     public String getCode() {
@@ -31,74 +20,44 @@ public class Alarm implements AlarmState, Actionable{
         }
     }
 
-<<<<<<< Updated upstream
-    public State getState() {
-        return state;
-    }
-
-    public void setState(State state) {
-        this.state = state;
-=======
     public AlarmState getState() {
         return alarmState;
     }
 
     public void setState(AlarmState alarmState) {
         this.alarmState = alarmState;
->>>>>>> Stashed changes
     }
 
     @Override
     public void activate(String code) {
-<<<<<<< Updated upstream
-        if (code == null && state instanceof AlarmAlert){
-            return;
-        }
-        this.state = new AlarmActive();
-=======
         if (code == null && alarmState instanceof AlarmAlert){
             return;
         }
         this.alarmState = new AlarmActive();
->>>>>>> Stashed changes
         this.code = code;
     }
 
     @Override
     public void deactivate(String code) {
-<<<<<<< Updated upstream
-        if (this.state instanceof AlarmDeactive)
+        if (code == null){
             return;
-        if (this.code.equals(code)) {
-            this.state = new AlarmDeactive();
-        } else {
-            this.state = new AlarmAlert();
-=======
+        }
         if (this.alarmState instanceof AlarmDeactive)
             return;
         if (this.code.equals(code)) {
             this.alarmState = new AlarmDeactive();
         } else {
             this.alarmState = new AlarmAlert();
->>>>>>> Stashed changes
         }
     }
 
     @Override
     public void alarmAlert() {
-<<<<<<< Updated upstream
-        this.state = new AlarmAlert();
-=======
         this.alarmState = new AlarmAlert();
->>>>>>> Stashed changes
     }
 
     @Override
     public void execute(Action action) {
-<<<<<<< Updated upstream
-        action.perform(this);
-=======
         action.act(this);
->>>>>>> Stashed changes
     }
 }
